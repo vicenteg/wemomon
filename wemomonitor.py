5bf3bd78a90c804a6600507f04980b36f123fa31
Vince Gonzalez <vince.gonzalez@gmail.com>
2017-01-06 13:07:01 -0500

    refine looping for use with supervisord

--- wemomonitor.py
+++ wemomonitor.py
@@ -27,28 +27,41 @@ def write_to_file(name, insight_params, path):
         json_data = "%s\n" % json.dumps({ "name": name, "timestamp": timestamp, "data":  data })
         f.write(json_data)
 
-def mainloop(names, dir, times=5, delay=10):
+
+def do(env, names, dir):
+    for name in names:
+        switch = env.get_switch(name)
+        write_to_file(name, switch.insight_params, dir)
+
+
+def mainloop(names, dir, times=0, delay=10):
     env = Environment(with_cache=False)
     env.start()
-    env.discover(10)
+    env.discover(5)
 
-    for i in range(0, times):
-        print >>sys.stderr, "Run %d of %d" % (i, times)
-        for name in names:
-            try:
-                switch = env.get_switch(name)
-                write_to_file(name, switch.insight_params, dir)
-            except (KeyboardInterrupt, SystemExit):
-                print "Goodbye!"
-                sys.exit(0)
-        time.sleep(delay)
+    if times < 1:
+        while True:
+            do(env,names,dir)
+            time.sleep(delay)
+    else:
+        for i in range(0, times):
+            do(env,names,dir)
+            time.sleep(delay)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Insight Poller")
+    parser.add_argument("--delay", type=int, default=10,
+                        help="delay between device queries.")
+    parser.add_argument("--times", type=int, default=0,
+                        help="number of times to query the device.")
     parser.add_argument("name", metavar="SWITCHNAME", nargs="+",
                         help="Names of the WeMo Insight switch(es)")
 
     parser.add_argument("--dir", help="Base directory to store data in", default="/tmp")
     args = parser.parse_args()
-    mainloop(args.name, args.dir)
+    try:
+        mainloop(args.name, args.dir, times=args.times, delay=args.delay)
+    except (KeyboardInterrupt,SystemExit) as ex:
+        print >>sys.stderr, "Exiting"
+        sys.exit(0)
